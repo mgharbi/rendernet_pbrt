1bc3dd780b5b0f89d24863b57a2bc6b70f53e702
Michael Gharbi <gharbi@mit.edu>
2018-04-03 11:57:05 -0400

    pathrendernet

--- src/renderers/rendernetrenderer.cpp
+++ src/renderers/rendernetrenderer.cpp
@@ -253,6 +253,7 @@ RendernetRenderer::RendernetRenderer(Sampler *s, Camera *c,
     useCameraSpaceNormals = useCamSpaceNrm;
 
     maxDepth = surfaceIntegrator->maxDepth();
+    printf("sI's maxdepth %d\n", maxDepth);
 }
 
 
@@ -370,7 +371,6 @@ Spectrum RendernetRenderer::RecordedLi(const Scene *scene,
           sr->radiance_diffuse.push_back(zero);
           sr->radiance_diffuse_indirect.push_back(zero);
           sr->radiance_specular.push_back(Li); // We only have the scene lights/envmap contributions
-          printf("using maxdepth %f\n", sr->maxDepth);
           std::vector<float> p(4*sr->maxDepth);
           sr->probabilities.push_back(p);
           std::vector<float> ld(2*sr->maxDepth);
